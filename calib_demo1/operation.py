633c51323e157439889f8c6f0804904b38d503d6
GimpelZhang <zjunchuan@gmail.com>
2020-03-06 14:04:52 +0800

    Apache web server application

--- calib_demo1/operation.py
+++ calib_demo1/operation.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-
+import paramiko
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from cmd_str.models import CMD_CONTENT
-import os
+# import os
 
 def world_start(request):
 	request.encoding = 'utf-8'
@@ -16,7 +16,17 @@ def world_start(request):
 		message = '你选择了'+str(request.POST['world_name'].encode('utf-8')) + ',你的信息录入完成。谢谢配合~'
 		test1 = CMD_CONTENT(world_name = request.POST['world_name'])
 		test1.save()
-		commandtest='cd ~/catkin_ws/;source ./devel/setup.bash;roslaunch octopus_pgm_map_creator start.launch'
-		commandtest='gnome-terminal -x bash -c "'+commandtest+';" &'
-		os.system(commandtest)
+		commandtest="cd calib_demo1/third_party;python3 -c 'import param1_calculation;print(param1_calculation.get_param(2))'"
+		ssh = paramiko.SSHClient()
+		ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+		ip1 = '10.1.14.135'
+		username1 = 'junchuan'
+		passwd1='simulation'
+		ssh.connect(ip1,22,username1,passwd1,timeout=5)
+		stdin, stdout, stderr = ssh.exec_command(commandtest)
+		out1=stdout.readlines()
+		message=message+out1[0]
+		ssh.close()       
 	return HttpResponse(message)
+# commandtest='cd /home/junchuan/catkin_ws/;source ./devel/setup.bash;roslaunch octopus_pgm_map_creator start.launch'
+# commandtest='gnome-terminal -x bash -c "'+commandtest+';" &'
